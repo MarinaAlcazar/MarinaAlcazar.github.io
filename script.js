3a0027dccb8caa35ab891b558249bde34795cd10
MarinaAlcazar <144031526+MarinaAlcazar@users.noreply.github.com>
2026-08-05 11:08:35 +0200

    Add files via upload
    updated files

---
+++ script.js
@@ -0,0 +1,10 @@
+/*This script might be useful in the future if I want to add features such as:
+
+- a dark/light mode toggle,
+- a "Back to top" button,
+- animations,
+- filtering publications,
+- an email contact form,
+- collapsible sections
+
+*/
